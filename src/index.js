20b2dbf7e344e326cfdba2d8e6715cc1b4208b4a
JerryPark <eaeao@naver.com>
2020-11-05 05:33:38 +0900

    react-navigation

--- src/index.js
+++ src/index.js
@@ -1,54 +1,19 @@
-import React from 'react';
-import {Pressable, SafeAreaView, Text} from 'react-native';
-import styled from 'styled-components/native';
-import {useObserver} from 'mobx-react';
-import useStore from './hooks/useStore';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import CounterScreen from './screens/counter';
 
-const Container = styled.View`
-  width: 100%;
-  height: 100%;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Title = styled.Text`
-  font-size: 30px;
-`;
+const Stack = createStackNavigator();
 
 const Index = () => {
-  const {auth, counter} = useStore();
-
-  const increase = () => {
-    counter.increase();
-  };
-
-  const decrease = () => {
-    counter.decrease();
-  };
-
-  const login = () => {
-    auth.login();
-  };
-
-  return useObserver(() => (
-    <>
-      <SafeAreaView>
-        <Container>
-          <Title>count : {counter.number}</Title>
-          <Pressable onPress={increase}>
-            <Text>+</Text>
-          </Pressable>
-          <Pressable onPress={decrease}>
-            <Text>-</Text>
-          </Pressable>
-          <Title style={{marginTop: 100}}>user : {auth.user}</Title>
-          <Pressable onPress={login}>
-            <Text>Login</Text>
-          </Pressable>
-        </Container>
-      </SafeAreaView>
-    </>
-  ));
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Counter">
+        <Stack.Screen name="Counter" component={CounterScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default Index;
---
+++ src/screens/counter.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {Pressable, SafeAreaView, Text} from 'react-native';
+import styled from 'styled-components/native';
+import {useObserver} from 'mobx-react';
+import useStore from '../hooks/useStore';
+
+const Container = styled.View`
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  font-size: 30px;
+`;
+
+const CounterScreen = () => {
+  const {auth, counter} = useStore();
+
+  const increase = () => {
+    counter.increase();
+  };
+
+  const decrease = () => {
+    counter.decrease();
+  };
+
+  const login = () => {
+    auth.login();
+  };
+
+  return useObserver(() => (
+    <>
+      <SafeAreaView>
+        <Container>
+          <Title>count : {counter.number}</Title>
+          <Pressable onPress={increase}>
+            <Text>+</Text>
+          </Pressable>
+          <Pressable onPress={decrease}>
+            <Text>-</Text>
+          </Pressable>
+          <Title style={{marginTop: 100}}>user : {auth.user}</Title>
+          <Pressable onPress={login}>
+            <Text>Login</Text>
+          </Pressable>
+        </Container>
+      </SafeAreaView>
+    </>
+  ));
+};
+
+export default CounterScreen;
